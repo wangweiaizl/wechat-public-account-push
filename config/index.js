cb3887a7e6782863176870500cf051ea61ed80bd
wangweiaizl <114322944+wangweiaizl@users.noreply.github.com>
2022-09-25 11:32:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -63,11 +63,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '老公',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_3eaac1117e5a',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'ofdom6H8rTcClvOtg8-i0wCxflDE',
       // 所在省份
       province: '广东',
       // 所在城市
